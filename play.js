fae77e7643c43a1bc0c4b55f46152a6be7523162
LiquidChris <cshokunb@uci.edu>
2020-12-06 21:13:37 -0800

    Implement Changable Mode and Make Play Button Clickable

--- play.js
+++ play.js
@@ -1,16 +1,16 @@
-/* play_NewRiff() is a placeholder because I haven't figured out 
+/* playNewRiff() is a placeholder because I haven't figured out 
 what I'm supposed to do with the play buttone to make it play
 any riff that might be made; abstraction is hard*/
 // delete comment later
-function play_NewRiff(){
+function playNewRiff(){
     
     if (mode === "playing") {  
         debug_PlayRiff()
-        setTimeout(()=>{play_NewRiff()}, 1600);
+        setTimeout(()=>{playNewRiff()}, 1600);
     }
 }
 
-function change_Mode() {
+function changeMode() {
     if (mode === "inputting") {
         mode = "playing"
         console.log(mode);
@@ -19,5 +19,5 @@ function change_Mode() {
         mode = "inputting" 
         console.log(mode);
     }
-    play_NewRiff();
+    playNewRiff();
 }
